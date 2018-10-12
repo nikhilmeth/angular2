a61564623408616b6589d66eddf0a0414a338e63
nikhil <nikhil.methuku@gmail.com>
2018-10-12 00:23:29 -0500

    adding component within component

---
+++ app/media-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mw-media-item',
+    templateUrl:  'app/media-item.component.html',
+    styleUrls: ['app/media-item.component.css']
+})
+
+export class MediaItemComponent { }
