4b18e9aca59a4394ab0d6edb0c33b0be4a9c4120
nikhil <nikhil.methuku@gmail.com>
2018-10-14 17:31:44 -0500

    injection token

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,10 +8,7 @@ import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
 import { MediaItemService } from './media-item.service';
-
-const lookupLists = {
-  medium:['Movie','Series']
-}
+import { lookupListToken, lookupLists} from './providers';
 
 @NgModule({
   imports: [
@@ -28,7 +25,7 @@ const lookupLists = {
   ],
   providers:[
     MediaItemService,
-    {provide: 'lookupListToken', useValue: lookupLists }
+    {provide: lookupListToken, useValue: lookupLists }
   ],
   bootstrap: [
     AppComponent
